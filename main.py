53d8552f315fe438569a67610a1c7817a2dfd84a
MatiasTieranta <71073868+MatiasTieranta@users.noreply.github.com>
2022-04-21 00:31:19 +0300

    Added Heroku support

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('Hello world from ci cd')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, world!"
+
+
+if __name__ == "__main__":
+    app.run()
